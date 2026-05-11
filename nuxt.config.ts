ca0a5265a77c77857d992b89ca6c6d576dbc43de
Kamil Gruca <kgruca7747@gmail.com>
2026-05-11 16:39:48 +0200

    feat(NEU-58): add satellite maps from Maptiler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,7 @@ export default defineNuxtConfig({
       apiBaseUrl: process.env.PUBLIC_API_BASE_URL || '/api',
       socketIoGatewayUrl: process.env.PUBLIC_SOCKET_IO_GATEWAY_URL,
       bciWsUrl: process.env.PUBLIC_BCI_WS_URL || 'ws://127.0.0.1:8765',
+      mapTilerApiKey: process.env.PUBLIC_MAPTILER_API_KEY || '',
     },
   },
   modules: [
